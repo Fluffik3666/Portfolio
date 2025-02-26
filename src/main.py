46f5b217e2c131a689db8749fc2bd58922ffa7fa
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2025-02-26 18:30:55 +0000

    Base code

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_from_directory, render_template
+
+app = Flask(__name__, template_folder='../src/templates', static_folder='../src/static')
+
+#! serve our important routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#! serve our static files
+#! routes go /content/static/<path>
+@app.route('/content/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080) # debug flag remove in prod or detect when running on vercel
